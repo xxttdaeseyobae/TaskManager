5d317810369544e1ef71354f585669d2fca5f842
Manisha <caseymansa@gmail.com>
2019-12-31 09:22:31 +0545

    Image Response

---
+++ app/src/main/java/com/example/taskmanager/strictmode/StrictModeClass.java
@@ -0,0 +1,13 @@
+package com.example.taskmanager.strictmode;
+
+public class StrictModeClass {
+
+    public static void StrictMode() {
+        android.os.StrictMode.ThreadPolicy policy =
+                new android.os.StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+
+        android.os.StrictMode.setThreadPolicy(policy);
+
+    }
+}
